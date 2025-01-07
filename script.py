d1b31ded7a794c22727894598bbc82a0ffba2183
muhammad-hamza-liaqat <mh408800@gmail.com>
2025-01-07 17:06:50 +0500

    env and requirements.txt

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-OPENAI_API_KEY = ""
-ELEVEN_LABS_API_KEY = ""
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
+print(OPENAI_API_KEY,ELEVEN_LABS_API_KEY, "from enviormental variables" )
 
 def get_chatgpt_response(prompt):
     url = "https://api.openai.com/v1/chat/completions"
